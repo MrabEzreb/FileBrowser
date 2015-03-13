2814304860ffdc4dd37d7f5b1b912455b4081ff9
bram.zerbe <bram.zerbe@E430-SPED-22.msddecatur.k12.in.us>
2015-03-13 14:28:34 -0400

    MADE IT MAVEN YAY

--- src/com/ezreb/filebrowser/FileBrowser.java
+++ src/com/ezreb/filebrowser/FileBrowser.java
@@ -1,23 +1,24 @@
 package com.ezreb.filebrowser;
 
-import java.awt.Desktop;
 import java.io.File;
-import java.io.IOException;
-
 import com.ezreb.filebrowser.images.ImageLoader;
 
 public class FileBrowser {
 
-	public static void main(String[] args) {
-		FileBrowser fb = new FileBrowser();
-		try {
-			File output = fb.run();
-			System.out.println("output: "+output.getAbsolutePath());
-			Desktop.getDesktop().open(output);
-		} catch(NullPointerException | IOException npe) {
-			
-		}
-	}
+//	public static void main(String[] args) {
+//		FileBrowser fb = new FileBrowser();
+//		try {
+//			File output = fb.run();
+//			System.out.println("output: "+output.getAbsolutePath());
+//			Desktop.getDesktop().open(output);
+//		} catch(NullPointerException | IOException npe) {
+//			
+//		}
+//	}
+	/**
+	 * Very simple to use. This method opens up a filebrowser window, and once a file is selected, the window closes and the file object is returned.
+	 * @return the chosen File
+	 */
 	public File run() {
 		ImageLoader.loadImages();
 		MainWindow mw = new MainWindow();
--- src/com/ezreb/filebrowser/package-info.java
+++ src/com/ezreb/filebrowser/package-info.java
@@ -1,7 +1,10 @@
 /**
- * The main package for my File Browser. To use it in your project, simply copy and paste this package.	
- */
-/**
+ * The main package for my File Browser.
+ * There are two ways to use this program.
+ * Maven (Recommended, also not sure if it works):
+ * Grab the most recent artifact from github and pull it in as a dependancy.
+ * Manual (working :P):
+ * To use this in your project, simply copy and paste this package.
  * @author bram.zerbe
  *
  */
